f72f383337876ea4e0931825c1b98d8d23b68722
Vincent Wong <duperduper@ucla.edu>
2014-04-03 23:33:58 -0700

    Ok NOW it compiles

--- lab1/execute-command.c
+++ lab1/execute-command.c
@@ -20,5 +20,6 @@ execute_command (command_t c, bool time_travel)
   /* FIXME: Replace this with your implementation.  You may need to
      add auxiliary functions and otherwise modify the source code.
      You can also use external functions defined in the GNU C Library.  */
+  c->status = (int)time_travel; //Placeholder, remove later
   error (1, 0, "command execution not yet implemented");
 }
