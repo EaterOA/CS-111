e46f5298bdf89d2c5a69a0a61915345796423b49
Vincent Wong <duperduper@ucla.edu>
2014-05-30 18:20:39 -0700

    Filled out most of answers.txt, fixed download buffer overflow

--- lab4/osppeer.c
+++ lab4/osppeer.c
@@ -516,7 +516,7 @@ task_t *start_download(task_t *tracker_task, const char *filename)
 		error("* Error while allocating task");
 		goto exit;
 	}
-	strcpy(t->filename, filename);
+	strncpy(t->filename, filename, FILENAMESIZ-1);
 
 	// add peers
 	s1 = buf;
@@ -574,7 +574,7 @@ static void task_download(task_t *t, task_t *tracker_task)
 	// at all.
 	for (i = 0; i < 50; i++) {
 		if (i == 0)
-			strcpy(t->disk_filename, t->filename);
+			strncpy(t->disk_filename, t->filename, FILENAMESIZ-1);
 		else
 			sprintf(t->disk_filename, "%s~%d~", t->filename, i);
 		t->disk_fd = open(t->disk_filename,
